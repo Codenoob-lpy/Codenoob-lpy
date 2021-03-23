d6141c56280bad5d2dc7c9384a3ce5d5cbff2d59
cyyjames <79643163+cyyjames@users.noreply.github.com>
2021-03-23 15:22:22 +0800

    Update newcharacter.cpp

---
+++ newcharacter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "functions.h"
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+using namespace std;
+
+void newcharacter()
+{
+    string name;
+    cout << "Character name: ";
+    cin >> name;
+    string newfilename =  name + ".txt";
+    ofstream newfile (newfilename);
+
+    int health = 100;
+    int hunger = 0;
+    int mana = 20;
+    double crit = 0;
+    
+    newfile << name << endl;
+    newfile << health << endl;
+    newfile << hunger << endl;
+    newfile << crit << endl;
+
+    newfile << "inventory slots: 10 starting from the next line" << endl;
+
+
+}
